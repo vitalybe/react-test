e26fbebdcb7bccd65da227dfdb1e856befb9f8b3
Vitaly Belman <vitaly@telefonica.com>
2017-11-30 12:13:31 +0200

    width isn't set for componenets

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ import "./App.css";
 import Swiper from "react-id-swiper";
 import "react-id-swiper/src/styles/css/swiper.css";
 
+function MyComponent(props) {
+  return <div className="slide">Awesome slide</div>
+}
+
 class App extends Component {
   render() {
     return (
       <div>
+        <h1>Div slides work as expected</h1>
         <Swiper>
-          <div className="slide">Slide 1</div>
-          <div className="slide">
-            Slide 2 <button onClick={() => alert("hi")}>Test</button>
-          </div>
-          <div className="slide">Slide 3</div>
+          <div className="slide">Awesome slide</div>
+          <div className="slide">Awesome slide</div>
+          <div className="slide">Awesome slide</div>
+        </Swiper>
+
+        <h1>Component slides do not work</h1>
+        <Swiper>
+          <MyComponent/>
+          <MyComponent/>
+          <MyComponent/>
+        </Swiper>
+
+        <h1>I can make it work by wrapping each slide with a div</h1>
+        <Swiper>
+          <div><MyComponent/></div>
+          <div><MyComponent/></div>
+          <div><MyComponent/></div>
         </Swiper>
       </div>
     );
