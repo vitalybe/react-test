8aed1da9075867cfb4ef1f555a72dd863969353b
Vitaly Belman <vitaly@telefonica.com>
2017-11-30 12:04:29 +0200

    blank react

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Swiper from "react-id-swiper";
-import "react-id-swiper/src/styles/css/swiper.css";
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Swiper>
-          <div className="slide">Slide 1</div>
-          <div className="slide">
-            Slide 2 <button onClick={() => alert("hi")}>Test</button>
-          </div>
-          <div className="slide">Slide 3</div>
-        </Swiper>
+        Hello react
       </div>
     );
   }
