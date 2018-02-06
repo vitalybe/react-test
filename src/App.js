1835960ca5fa54af3989f0d6d6c97368b7d9f428
Vitaly <vitalyb@gmail.com>
2018-02-06 15:30:13 +0200

    mobx app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { observer } from "mobx-react";
+import { computed, observable } from "mobx";
 
-class App extends Component {
+@observer
+class Todo extends Component {
+  render() {
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.todo.done}
+          onChange={() => (this.props.todo.done = !this.props.todo.done)}
+        />
+        {this.props.todo.title}
+      </li>
+    );
+  }
+}
+
+@observer
+class Todos extends Component {
   render() {
     return (
       <div>
-        Hello react
+        <ul>{this.props.appState.todos.map((todo, idx) => <Todo todo={todo} key={idx} />)}</ul>
+        Remaining: {this.props.appState.left}
+        <button onClick={() => alert("hi")}>Modify stuff</button>
       </div>
     );
+  }
+}
+
+class State {
+  @observable
+  todos = [
+    {
+      title: "Create MobX intro video",
+      done: false,
+    },
+    {
+      title: "Take a nap",
+      done: true,
+    },
+  ];
+
+  @computed
+  get left() {
+    return this.todos.filter(todo => !todo.done).length;
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.appState = new State();
+  }
+
+  render() {
+    return <Todos appState={this.appState} />;
   }
 }
 
