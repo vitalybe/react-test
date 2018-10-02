1ca99131e7e9064c9a52748a6de0939f503553f1
Vitaly <vitalyb@gmail.com>
2018-10-02 14:26:52 +0300

    basic ui without animations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import posed, { PoseGroup } from "react-pose";
 import "./App.css";
+
+
+class Tooltip extends Component {
+  render() {
+    return <div className="tooltip" />;
+  }
+}
+
+class Marker extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div
+        className="marker"
+        onMouseEnter={() => this.setState({ mouseOver: true })}
+        onMouseLeave={() => this.setState({ mouseOver: false })}>
+        {this.state.mouseOver ? <Tooltip /> : null}
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello react
+      <div className="landscape">
+        <Marker />
+        <Marker />
+        <Marker />
       </div>
     );
   }
